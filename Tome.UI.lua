887deb6042c2982c3647a88c1933c392b62a9b23
Zhevron <thomas@lokshall.com>
2013-06-17 17:36:25 +0200

    Added age, height and weight fields

--- Tome.UI.lua
+++ Tome.UI.lua
@@ -117,6 +117,9 @@ function Tome.UI.Event_Button_Save(handle)
     Tome_Character.Name = string.gsub(Tome.UI.Layouts.Character.Name:GetText(), "Name", "")
     Tome_Character.Suffix = string.gsub(Tome.UI.Layouts.Character.Suffix:GetText(), "Suffix", "")
     Tome_Character.Title = string.gsub(Tome.UI.Layouts.Character.Title:GetText(), "Title", "")
+    Tome_Character.Age = string.gsub(Tome.UI.Layouts.Character.Age:GetText(), "Age", "")
+    Tome_Character.Height = string.gsub(Tome.UI.Layouts.Character.Height:GetText(), "Height", "")
+    Tome_Character.Weight = string.gsub(Tome.UI.Layouts.Character.Weight:GetText(), "Weight", "")
     Tome_Character.InCharacter = string.find(Tome.UI.Layouts.Character.InCharacter:GetText(), "IC") and true or false
     Tome_Character.Tutor = string.find(Tome.UI.Layouts.Character.Tutor:GetText(), "Tutor: On") and true or false
     Tome_Character.Flag = Tome.UI.Layouts.Character.Flag:GetSelectedValue()
@@ -137,6 +140,9 @@ function Tome.UI.Event_Button_Close(handle)
     Tome.UI.Layouts.Character.Name:SetKeyFocus(false)
     Tome.UI.Layouts.Character.Suffix:SetKeyFocus(false)
     Tome.UI.Layouts.Character.Title:SetKeyFocus(false)
+    Tome.UI.Layouts.Character.Age:SetKeyFocus(false)
+    Tome.UI.Layouts.Character.Height:SetKeyFocus(false)
+    Tome.UI.Layouts.Character.Weight:SetKeyFocus(false)
     Tome.UI.Layouts.Character.Appearance.Text:SetKeyFocus(false)
     Tome.UI.Layouts.Character.History.Text:SetKeyFocus(false)
 
@@ -153,6 +159,9 @@ function Tome.UI.Show(data)
         --Tome.UI.Layouts.Character.Name:SetEnabled(false)
         --Tome.UI.Layouts.Character.Suffix:SetEnabled(false)
         --Tome.UI.Layouts.Character.Title:SetEnabled(false)
+        --Tome.UI.Layouts.Character.Age:SetEnabled(false)
+        --Tome.UI.Layouts.Character.Height:SetEnabled(false)
+        --Tome.UI.Layouts.Character.Weight:SetEnabled(false)
         --Tome.UI.Layouts.Character.InCharacter:SetEnabled(false)
         --Tome.UI.Layouts.Character.Tutor:SetEnabled(false)
         --Tome.UI.Layouts.Character.Flag:SetEnabled(false)
@@ -167,6 +176,9 @@ function Tome.UI.Show(data)
         --Tome.UI.Layouts.Character.Name:SetEnabled(true)
         --Tome.UI.Layouts.Character.Suffix:SetEnabled(true)
         --Tome.UI.Layouts.Character.Title:SetEnabled(true)
+        --Tome.UI.Layouts.Character.Age:SetEnabled(true)
+        --Tome.UI.Layouts.Character.Height:SetEnabled(true)
+        --Tome.UI.Layouts.Character.Weight:SetEnabled(true)
         --Tome.UI.Layouts.Character.InCharacter:SetEnabled(true)
         --Tome.UI.Layouts.Character.Tutor:SetEnabled(true)
         --Tome.UI.Layouts.Character.Flag:SetEnabled(true)
@@ -179,6 +191,9 @@ function Tome.UI.Show(data)
     Tome.UI.Layouts.Character.Name:SetText((data.Name ~= "") and data.Name or "Name")
     Tome.UI.Layouts.Character.Suffix:SetText((data.Suffix ~= "") and data.Suffix or "Suffix")
     Tome.UI.Layouts.Character.Title:SetText((data.Title ~= "") and data.Title or "Title")
+    Tome.UI.Layouts.Character.Age:SetText((data.Age ~= "") and data.Age or "Age")
+    Tome.UI.Layouts.Character.Height:SetText((data.Height ~= "") and data.Height or "Height")
+    Tome.UI.Layouts.Character.Weight:SetText((data.Weight ~= "") and data.Weight or "Weight")
     Tome.UI.Layouts.Character.InCharacter:SetText(data.InCharacter and "IC" or "OOC")
     Tome.UI.Layouts.Character.Tutor:SetText(data.Tutor and "Tutor: On" or "Tutor: Off")
     Tome.UI.Layouts.Character.Flag:SetSelectedValue(data.Flag)
@@ -381,8 +396,8 @@ function Tome.UI.CreateCharacterLayout()
 
     -- Create the Title field
     Tome.UI.Layouts.Character.Title = UI.CreateFrame("RiftTextfield", "Tome_UI_Layout_Character_Title", Tome.UI.Layouts.Character)
-    Tome.UI.Layouts.Character.Title:SetPoint("TOPLEFT", Tome.UI.Layouts.Character.Prefix, "BOTTOMLEFT", 0, 10)
-    Tome.UI.Layouts.Character.Title:SetPoint("TOPRIGHT", Tome.UI.Layouts.Character.Suffix, "BOTTOMRIGHT", 0, 10)
+    Tome.UI.Layouts.Character.Title:SetPoint("TOPLEFT", Tome.UI.Layouts.Character.Prefix, "BOTTOMLEFT", 0, 5)
+    Tome.UI.Layouts.Character.Title:SetPoint("TOPRIGHT", Tome.UI.Layouts.Character.Suffix, "BOTTOMRIGHT", 0, 5)
     Tome.UI.Layouts.Character.Title:SetBackgroundColor(0.0, 0.0, 0.0, 0.7)
     Tome.UI.Layouts.Character.Title:SetText("Title")
     Tome.UI.Layouts.Character.Title:EventAttach(
@@ -412,6 +427,112 @@ function Tome.UI.CreateCharacterLayout()
             Tome.UI.NavButtons.Save:SetEnabled(true)
         end,
         "Tome_UI_Layout_Character_Title_Change"
+    )
+
+    -- Create the Age field
+    Tome.UI.Layouts.Character.Age = UI.CreateFrame("RiftTextfield", "Tome_UI_Layout_Character_Age", Tome.UI.Layouts.Character)
+    Tome.UI.Layouts.Character.Age:SetPoint("TOPLEFT", Tome.UI.Layouts.Character.Title, "BOTTOMLEFT", 0, 5)
+    Tome.UI.Layouts.Character.Age:SetWidth(size)
+    Tome.UI.Layouts.Character.Age:SetBackgroundColor(0.0, 0.0, 0.0, 0.7)
+    Tome.UI.Layouts.Character.Age:SetText("Age")
+    Tome.UI.Layouts.Character.Age:EventAttach(
+        Event.UI.Input.Key.Focus.Gain,
+        function(handle)
+            -- Clear the text if it matches default
+            if Tome.UI.Layouts.Character.Age:GetText() == "Age" then
+                Tome.UI.Layouts.Character.Age:SetText("")
+            end
+        end,
+        "Tome_UI_Layout_Character_Age_Focus_Gain"
+    )
+    Tome.UI.Layouts.Character.Age:EventAttach(
+        Event.UI.Input.Key.Focus.Loss,
+        function(handle)
+            -- Set the default text if field is empty
+            if Tome.UI.Layouts.Character.Age:GetText() == "" then
+                Tome.UI.Layouts.Character.Age:SetText("Age")
+            end
+        end,
+        "Tome_UI_Layout_Character_Age_Focus_Loss"
+    )
+    Tome.UI.Layouts.Character.Age:EventAttach(
+        Event.UI.Input.Key.Up,
+        function(handle, key)
+            -- Enable the Save button
+            Tome.UI.NavButtons.Save:SetEnabled(true)
+        end,
+        "Tome_UI_Layout_Character_Age_Change"
+    )
+
+    -- Create the Weight field
+    Tome.UI.Layouts.Character.Weight = UI.CreateFrame("RiftTextfield", "Tome_UI_Layout_Character_Weight", Tome.UI.Layouts.Character)
+    Tome.UI.Layouts.Character.Weight:SetPoint("TOPRIGHT", Tome.UI.Layouts.Character.Title, "BOTTOMRIGHT", 0, 5)
+    Tome.UI.Layouts.Character.Weight:SetWidth(size)
+    Tome.UI.Layouts.Character.Weight:SetBackgroundColor(0.0, 0.0, 0.0, 0.7)
+    Tome.UI.Layouts.Character.Weight:SetText("Weight")
+    Tome.UI.Layouts.Character.Weight:EventAttach(
+        Event.UI.Input.Key.Focus.Gain,
+        function(handle)
+            -- Clear the text if it matches default
+            if Tome.UI.Layouts.Character.Weight:GetText() == "Weight" then
+                Tome.UI.Layouts.Character.Weight:SetText("")
+            end
+        end,
+        "Tome_UI_Layout_Character_Weight_Focus_Gain"
+    )
+    Tome.UI.Layouts.Character.Weight:EventAttach(
+        Event.UI.Input.Key.Focus.Loss,
+        function(handle)
+            -- Set the default text if field is empty
+            if Tome.UI.Layouts.Character.Weight:GetText() == "" then
+                Tome.UI.Layouts.Character.Weight:SetText("Weight")
+            end
+        end,
+        "Tome_UI_Layout_Character_Weight_Focus_Loss"
+    )
+    Tome.UI.Layouts.Character.Weight:EventAttach(
+        Event.UI.Input.Key.Up,
+        function(handle, key)
+            -- Enable the Save button
+            Tome.UI.NavButtons.Save:SetEnabled(true)
+        end,
+        "Tome_UI_Layout_Character_Weight_Change"
+    )
+
+    -- Create the Height field
+    Tome.UI.Layouts.Character.Height = UI.CreateFrame("RiftTextfield", "Tome_UI_Layout_Character_Height", Tome.UI.Layouts.Character)
+    Tome.UI.Layouts.Character.Height:SetPoint("TOPLEFT", Tome.UI.Layouts.Character.Age, "TOPRIGHT", 5, 0)
+    Tome.UI.Layouts.Character.Height:SetPoint("TOPRIGHT", Tome.UI.Layouts.Character.Weight, "TOPLEFT", -5, 0)
+    Tome.UI.Layouts.Character.Height:SetWidth(size)
+    Tome.UI.Layouts.Character.Height:SetBackgroundColor(0.0, 0.0, 0.0, 0.7)
+    Tome.UI.Layouts.Character.Height:SetText("Height")
+    Tome.UI.Layouts.Character.Height:EventAttach(
+        Event.UI.Input.Key.Focus.Gain,
+        function(handle)
+            -- Clear the text if it matches default
+            if Tome.UI.Layouts.Character.Height:GetText() == "Height" then
+                Tome.UI.Layouts.Character.Height:SetText("")
+            end
+        end,
+        "Tome_UI_Layout_Character_Height_Focus_Gain"
+    )
+    Tome.UI.Layouts.Character.Height:EventAttach(
+        Event.UI.Input.Key.Focus.Loss,
+        function(handle)
+            -- Set the default text if field is empty
+            if Tome.UI.Layouts.Character.Height:GetText() == "" then
+                Tome.UI.Layouts.Character.Height:SetText("Height")
+            end
+        end,
+        "Tome_UI_Layout_Character_Height_Focus_Loss"
+    )
+    Tome.UI.Layouts.Character.Height:EventAttach(
+        Event.UI.Input.Key.Up,
+        function(handle, key)
+            -- Enable the Save button
+            Tome.UI.NavButtons.Save:SetEnabled(true)
+        end,
+        "Tome_UI_Layout_Character_Height_Change"
     )
 
     size = (((Tome.UI.Layouts.Character:GetWidth() / 100) * 35) - 10) / 2
@@ -484,13 +605,13 @@ function Tome.UI.CreateCharacterLayout()
     -- Create the Appearance field
     Tome.UI.Layouts.Character.Appearance = {}
     Tome.UI.Layouts.Character.Appearance.Label = UI.CreateFrame("Text", "Tome_UI_Layout_Character_Appearance_Label", Tome.UI.Layouts.Character)
-    Tome.UI.Layouts.Character.Appearance.Label:SetPoint("TOPLEFT", Tome.UI.Layouts.Character.Title, "BOTTOMLEFT", 0, 10)
-    Tome.UI.Layouts.Character.Appearance.Label:SetPoint("TOPRIGHT", Tome.UI.Layouts.Character.Flag, "BOTTOMRIGHT", 0, 10)
+    Tome.UI.Layouts.Character.Appearance.Label:SetPoint("TOPLEFT", Tome.UI.Layouts.Character.Age, "BOTTOMLEFT", 0, 10)
+    --Tome.UI.Layouts.Character.Appearance.Label:SetPoint("TOPRIGHT", Tome.UI.Layouts.Character.Flag, "BOTTOMRIGHT", 0, 10)
     Tome.UI.Layouts.Character.Appearance.Label:SetText("Appearance:")
     Tome.UI.Layouts.Character.Appearance.Text = UI.CreateFrame("SimpleTextArea", "Tome_UI_Layout_Character_Appearance_Text", Tome.UI.Layouts.Character)
     Tome.UI.Layouts.Character.Appearance.Text:SetPoint("TOPLEFT", Tome.UI.Layouts.Character.Appearance.Label, "BOTTOMLEFT", 0, 5)
     Tome.UI.Layouts.Character.Appearance.Text:SetPoint("TOPRIGHT", Tome.UI.Layouts.Character.Appearance.Label, "BOTTOMRIGHT", 0, 5)
-    Tome.UI.Layouts.Character.Appearance.Text:SetHeight((Tome.UI.Window:GetContent():GetHeight() / 100) * 30)
+    Tome.UI.Layouts.Character.Appearance.Text:SetHeight((Tome.UI.Window:GetContent():GetHeight() / 100) * 25)
     Tome.UI.Layouts.Character.Appearance.Text:SetBackgroundColor(0.0, 0.0, 0.0, 0.7)
     Tome.UI.Layouts.Character.Appearance.Text:EventAttach(
         Event.UI.Input.Key.Up,
@@ -510,7 +631,7 @@ function Tome.UI.CreateCharacterLayout()
     Tome.UI.Layouts.Character.History.Text = UI.CreateFrame("SimpleTextArea", "Tome_UI_Layout_Character_History_Text", Tome.UI.Layouts.Character)
     Tome.UI.Layouts.Character.History.Text:SetPoint("TOPLEFT", Tome.UI.Layouts.Character.History.Label, "BOTTOMLEFT", 0, 5)
     Tome.UI.Layouts.Character.History.Text:SetPoint("TOPRIGHT", Tome.UI.Layouts.Character.History.Label, "BOTTOMRIGHT", 0, 5)
-    Tome.UI.Layouts.Character.History.Text:SetHeight((Tome.UI.Window:GetContent():GetHeight() / 100) * 30)
+    Tome.UI.Layouts.Character.History.Text:SetHeight((Tome.UI.Window:GetContent():GetHeight() / 100) * 25)
     Tome.UI.Layouts.Character.History.Text:SetBackgroundColor(0.0, 0.0, 0.0, 0.7)
     Tome.UI.Layouts.Character.History.Text:EventAttach(
         Event.UI.Input.Key.Up,
