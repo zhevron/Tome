9adf58dc8f3f5b91b239c8a65b8d7ce1dc73a38c
Zhevron <thomas@lokshall.com>
2013-07-08 13:58:51 +0200

    Since the last fix didn't work, we'll try this

--- Tome.Widget.TextArea.lua
+++ Tome.Widget.TextArea.lua
@@ -66,13 +66,6 @@ function Tome.Widget.TextArea.Create(parent, name, editable, callback)
     -- Set the width of the text field frame
     widget.Textfield:SetWidth(widget.Container:GetWidth() - widget.Scrollbar:GetWidth())
 
-    -- Attach to the focus gain event of the mask frame
-    widget.Mask:EventAttach(
-        Event.UI.Input.Key.Focus.Gain,
-        Tome.Widget.TextArea.Event_Mask_Focus_Gain,
-        string.format("%s_Mask_Event_Focus_Gain", name)
-    )
-
     -- Attach to the scrollwheel up event of the container frame
     widget.Container:EventAttach(
         Event.UI.Input.Mouse.Wheel.Forward,
@@ -94,12 +87,19 @@ function Tome.Widget.TextArea.Create(parent, name, editable, callback)
         string.format("%s_Scrollbar_Event_Changed", name)
     )
 
-    -- Attach to the keyboard key up event of the textfield frame if editable
     if widget.Editable then
+        -- Attach to the keyboard key up event of the textfield frame
         widget.Textfield:EventAttach(
             Event.UI.Input.Key.Up,
             Tome.Widget.TextArea.Event_Textfield_KeyUp,
             string.format("%s_Textfield_Event_KeyUp", name)
+        )
+
+        -- Attach to the left mouse click event of the container frame
+        widget.Container:EventAttach(
+            Event.UI.Input.Mouse.Left.Click,
+            Tome.Widget.TextArea.Event_Container_LeftMouse,
+            string.format("%s_Container_Event_LeftMouse", name)
         )
     end
 
@@ -265,8 +265,8 @@ function Tome.Widget.TextArea.UpdatePosition(self)
     self.Textfield:SetPoint("TOPLEFT", self.Container, "TOPLEFT", 0, -self.Offset)
 end
 
--- This function is fired by the event API when the mask frame gains focus
-function Tome.Widget.TextArea.Event_Mask_Focus_Gain(handle)
+-- This function is fired by the event API when the left mouse button is pressed inside the container frame
+function Tome.Widget.TextArea.Event_Container_LeftMouse(handle)
     -- Set the focus to the textfield
     handle:getParent().Widget.Textfield:SetKeyFocus(true)
 end
