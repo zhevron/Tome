7d00a1e407e0b769efdd230f007d3804b33ef340
Zhevron <thomas@lokshall.com>
2013-08-05 19:11:00 +0200

    Tome_Character is now set by defaults to ensure the saved variables are up to date with the addon

--- Tome.Defaults.lua
+++ Tome.Defaults.lua
@@ -29,7 +29,7 @@ Tome_Defaults = {
     }
 }
 
-Tome_Character = {
+Tome_Character_Defaults = {
     Prefix = detail and detail.titlePrefixName or "",
     Name = detail and detail.name or "",
     Suffix = detail and detail.titleSuffixName or "",
@@ -53,3 +53,5 @@ Tome_Cache = {}
 Tome_Throttle = {}
 
 Tome_Config = Tome_Defaults
+
+Tome_Character = Tome_Character_Defaults
