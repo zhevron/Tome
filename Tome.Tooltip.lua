6e2635bcc7378fec457edf83a6d7ab0a9cdf7769
Rhundas <niandria@hotmail.co.uk>
2013-08-24 05:32:53 +0100

    I guess it would be useful if we had something to change the name colours BACK.

--- Tome.Tooltip.lua
+++ Tome.Tooltip.lua
@@ -146,6 +146,8 @@ function Tome.Tooltip.Update(data)
 		Tome.Tooltip.Name:SetFontColor(1.0, 0.0, 0.8, 1.0)
 	elseif Tome.Tooltip.Target.name == "Lahni" then
 		Tome.Tooltip.Name:SetFontColor(0.6, 0.0, 0.8, 1.0)
+	else
+		Tome.Tooltip.Name:SetFontColor(1.0, 1.0, 1.0, 1.0)
 	end
 
     -- Make a temporary variable to store our flag text
