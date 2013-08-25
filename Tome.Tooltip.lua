2638ea9622ce2c6b626f67f4792e6a101e8233c6
Rhundas <niandria@hotmail.co.uk>
2013-08-25 01:23:40 +0100

    Removed developer colours from master, as they'll be merged from the tooltip bugfix.

--- Tome.Tooltip.lua
+++ Tome.Tooltip.lua
@@ -140,16 +140,7 @@ function Tome.Tooltip.Update(data)
 
     -- Set the players name
     Tome.Tooltip.Name:SetText(name)
-		if Tome.Tooltip.Target.name == "Maiiel" then
-		Tome.Tooltip.Name:SetFontColor(1.0, 0.4, 0.0, 1.0)
-	elseif Tome.Tooltip.Target.name == "Lyrai" then
-		Tome.Tooltip.Name:SetFontColor(1.0, 0.0, 0.8, 1.0)
-	elseif Tome.Tooltip.Target.name == "Lahni" then
-		Tome.Tooltip.Name:SetFontColor(0.6, 0.0, 0.8, 1.0)
-	else
-		Tome.Tooltip.Name:SetFontColor(1.0, 1.0, 1.0, 1.0)
-	end
-
+	
     -- Make a temporary variable to store our flag text
     local flagtext = ""
 
